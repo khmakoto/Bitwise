59daae987f9ae34043c988a2d335c0ac6187cb4d
Ricardo <ricardo.perezper@gmail.com>
2015-11-08 11:32:35 -0600

    a

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -10,9 +10,22 @@ class PublicationsController < ApplicationController
   end
 
   def new
+    @publication = Publication.new
+    @type = params[:type]
+    if @type == "article"
+      render "new_article.html.erb"
+    else
+      render "new_review.html.erb"
+    end
   end
 
   def create
+    exit
+
   end
+  private
+    def publication_params
+      params.require(:publication).permit(:title, :type, :section, :intro, :content, :summary, :grade)
+    end
 
 end
